65dd82f7de6f81c96984755b5ec23c251874bb39
votejeb <88135@farnborough.ac.uk>
2019-09-29 17:47:44 +0100

    Continued work on database

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 import org.sqlite.SQLiteConfig;
 
-import javax.print.attribute.standard.DateTimeAtCompleted;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class Main {
 
@@ -10,6 +12,9 @@ public class Main {
     public static void main(String[] args) {
         openDatabase("Database1.db");
 // code to get data from, write to the database etc goes here!
+        ShowUserInfo();
+        InsertUser(2,"test2","test2");
+        updateUser(2,"test3","test3");
         closeDatabase();
     }
 //This subroutine opens the database connection, outputs any errors with try catch statement
@@ -39,7 +44,7 @@ public class Main {
     //SQL SELECT//
     public static void ShowUserInfo() {
         try {
-            PreparedStatement ps = db.prepareStatement("SELECT UserID, UserName, TimeTo FROM Users");
+            PreparedStatement ps = db.prepareStatement("SELECT UserID, UserName, Password FROM Users");
 
             ResultSet results  = ps.executeQuery();
             while (results.next()) {
@@ -52,5 +57,39 @@ public class Main {
             System.out.println("Database error: " + exception.getMessage());
         }
     }
+//insert//
+
+    public static void InsertUser(int UserID, String UserName, String Password){
+        try {
+            PreparedStatement ps = db.prepareStatement("INSERT INTO Users(UserID, UserName, Password)VALUES(?,?,?)");
+            ps.setInt(1, UserID);
+            ps.setString(2, UserName);
+            ps.setString(3, Password);
+            ps.executeUpdate();
+            System.out.println("User Added");
+
+        } catch (Exception exception) {
+            System.out.println("Database error"+exception.getMessage());
+        }
+    }
+
+    //Update//
+    public static void updateUser (int UserID, String UserName, String Password){
+        try {
+
+            PreparedStatement ps = db.prepareStatement("UPDATE Users SET UserName = ?, String Password = ? WHERE UserID = ?");
+            ps.setInt(1, UserID);
+            ps.setString(2, UserName);
+            ps.setString(3, Password);
+            ps.executeUpdate();
+
+        } catch (Exception exception) {
+
+            System.out.println("Database Error"+exception.getMessage());
+
+        }
+
+    }
+
 
 }
