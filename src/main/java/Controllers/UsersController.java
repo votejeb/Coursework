c9a72d34852987adefb7bd3e025c2bfdeda9ccd4
votejeb <88135@farnborough.ac.uk>
2019-10-03 16:09:42 +0100

    kermit and push

--- src/main/java/Controllers/UsersController.java
+++ src/main/java/Controllers/UsersController.java
@@ -1,21 +1,42 @@
+package Controllers;
+
+import Server.Main;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+
+@Path("users/")
 public class UsersController {
 
     //SQL SELECT//
-    public static void ShowUserInfo() {
+    @GET
+    @Path("list")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String ShowUserInfo() {
+        System.out.println("thing/list");
+        JSONArray list = new JSONArray();
         try {
             PreparedStatement ps = Main.db.prepareStatement("SELECT UserID, UserName, Password FROM Users");
             ResultSet results  = ps.executeQuery();
             while (results.next()) {
-                int UserID = results.getInt(1);
-                String UserName = results.getString(2);
-                System.out.println(UserID + " " + UserName);
+                JSONObject item = new JSONObject();
+                item.put("UserID", results.getInt(1));
+                item.put("UserName", results.getString(2));
+                item.put("Password", results.getString(3));
+                list.add(item);
             }
-
+            return list.toString();
         } catch (Exception exception) {
             System.out.println("Database error: " + exception.getMessage());
+            return "{\"error\": \"Unable to list items, please see server console for more info.\"}";
+
         }
     }
 //insert//
