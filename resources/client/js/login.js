162c9eaee831e9e6a13ccf2b8949549ff7f200f6
votejeb <88135@farnborough.ac.uk>
2020-01-27 12:16:49 +0000

    charts js workds

--- resources/client/js/login.js
+++ resources/client/js/login.js
@@ -1,9 +1,11 @@
 function pageLoad() {
-
+    //checks login status
     if(window.location.search === '?logout') {
+        //logs user out
         document.getElementById('content').innerHTML = '<h1>Logging out, please wait...</h1>';
         logout();
     } else {
+        //logs user in and gives login form when event is called
         document.getElementById("loginButton").addEventListener("click", login);
     }
 
@@ -12,9 +14,10 @@ function pageLoad() {
 function login(event) {
 
     event.preventDefault();
-
+    //fetches form data
     const form = document.getElementById("loginForm");
     const formData = new FormData(form);
+    //calls server function to login
     fetch("/users/login", {method: 'post', body: formData}
     ).then(response => response.json()
     ).then(responseData => {
@@ -22,17 +25,18 @@ function login(event) {
         if (responseData.hasOwnProperty('error')) {
             alert(responseData.error);
         } else {
+            //sets site cookies
             Cookies.set("UserName", responseData.username);
             Cookies.set("token", responseData.token);
             Cookies.set("userid", responseData.userid);
-
+            //redirects page
             window.location.href = '/client/index.html';
         }
     });
 }
-
+//logout function
 function logout() {
-
+    //removes cookies and token from database
     fetch("/users/logout", {method: 'post'}
     ).then(response => response.json()
     ).then(responseData => {
--- resources/client/js/process.js
+++ resources/client/js/process.js
@@ -1,11 +1,13 @@
 function pageLoad(){
+    //view different relevant tables
     viewFilterTable(Cookies.get("userid"));
     viewDataTable(Cookies.get("userid"));
+    // draw graph event listener
     document.getElementById("drawGraph").addEventListener("click", drawGraph);
 }
 
 function drawGraph(event) {
-
+//configuration constructor for graph
     var config={
         type: 'line',
         data: {
@@ -34,31 +36,33 @@ function drawGraph(event) {
     };
 
     event.preventDefault();
-
+    //creates formdata object
     const form = document.getElementById("graphForm");
     const formData = new FormData(form);
-
+    //gets setid form formdata
     var tableID = formData.get("SetID");
-
+    //fetches dataset information from the server to search through
     fetch("/datasets/listone/"+Cookies.get("userid"),{method:'get'}
     ).then(response=>response.json()
     ).then(responseData=> {
         for (var index4 = 0; index4 < responseData.length; index4++) {
+            //fetches rawset data
             if (responseData[index4].SetID===parseInt(tableID,10)){
                 var RawSets=responseData[index4].RawSets;
-
+                //builds mychart object
                 var ctx = document.getElementById('chartCanvas').getContext('2d');
                 var myChart = new Chart(ctx, config);
                 console.log(RawSets);
+                //reads processed data table
                 fetch("/processeddatas/readkeywords/"+tableID.toString()+"/"+RawSets, {method: 'get'}
                 ).then(response => response.json()
                 ).then(responseData => {
-
+                    //converts sttring to arrayand iteates over each one t ogive x axis values
                     var RawSets1=RawSets.split("-");
                     for (var index3 = 0; index3 < RawSets1.length; index3++) {
                         config.data.labels.push(RawSets1[index3]);
                     }
-
+                    //iterates over each value of json lis and tehm to table in teh correct order
                     for (var index2 = 0; index2 < responseData.length; index2++) {
                         var datas=responseData[index2];
                         var newDataset = {
@@ -69,14 +73,18 @@ function drawGraph(event) {
                             fill: false
                         };
                         newDataset.label.push(datas.Words);
+                        //converts object to array
                         var jeff=datas.WordCount.substring(1, datas.WordCount.length-1).split(", " ).map(function(item) {
                             return parseInt(item, 10);
                         });
+                        //pushes variable to config
                         for (var index = 0; index < jeff.length; index++) {
                             newDataset.data.push(jeff[index]);
                         }
+                        //pushes new dataset
                         config.data.datasets.push(newDataset);
                     }
+                    //updates chart instance
                     myChart.update();
                 });
             } else {
@@ -86,7 +94,7 @@ function drawGraph(event) {
     });
 
 }
-
+//random colour generator function
 function getRandomColour() {
     var letters = '0123456789ABCDEF';
     var color = '#';
@@ -95,13 +103,15 @@ function getRandomColour() {
     }
     return color;
 }
-
+//view dataset table function
 function viewDataTable(UserID){
+    //builds table layout
     let tableHTML = '<table>' +
         '<tr>' +
         '<th>SetID|</th>' +
         '<th>Keyword</th>' +
         '</tr>';
+    //fetches data from dataset
     fetch("/datasets/listone/"+UserID,{method:'get'}
     ).then(response=>response.json()
     ).then(responseData=> {
@@ -109,6 +119,7 @@ function viewDataTable(UserID){
         if (responseData.hasOwnProperty('error')) {
             alert(responseData.error);
         } else {
+            //HTML constructor
             for (var i=0; i<responseData.length; i++){
                 tableHTML += `<tr>` +
                     `<td>${responseData[i].SetID}|</td>` +
@@ -120,8 +131,9 @@ function viewDataTable(UserID){
         document.getElementById("tableHTML").innerHTML = tableHTML;
     })
 }
-
+//view applicable filters to apply
 function viewFilterTable(UserID){
+    //builds table layout
     let filterHTML = '<table>' +
         '<tr>' +
         '<th>Data Filter ID|</th>' +
@@ -129,6 +141,7 @@ function viewFilterTable(UserID){
         '<th>Whitelist Status|</th>' +
         '<th>Public Status</th>' +
         '</tr>';
+    //fetches data from dataset
     fetch("/datafilters/listone/"+UserID,{method:'get'}
     ).then(response=>response.json()
     ).then(responseData=> {
@@ -138,6 +151,7 @@ function viewFilterTable(UserID){
         } else {
             for (var i=0; i<responseData.length; i++){
                 filterHTML += `<tr>` +
+                    //HTML constructor
                     `<td>${responseData[i].DataFilterID}|</td>` +
                     `<td>${responseData[i].DataFilterName}|</td>` +
                     `<td>${responseData[i].WhitelistBlacklist}|</td>` +
